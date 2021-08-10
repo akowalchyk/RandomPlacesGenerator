0d59d5153228f56dd5a8976cb020d87c4d68331f
Adam Kowalchyk <akowalchyk@zagmail.gonzaga.edu>
2021-08-10 16:44:39 -0700

    please

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ def generate():
     distance =  request.form.get('distance')
     address = str(address)
     distance = str(distance)
-    google_url, photo_url, name, type = get_random_place(address, distance)
-
+    google_url, photo_url, name, type = "hello"
     return render_template('index.html', google_url = google_url, photo_url = photo_url, name = name, type = type)
 
 
