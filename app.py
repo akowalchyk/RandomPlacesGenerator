b060ae2fa1c16c593c3e0f6c1350c79449a06818
Adam Kowalchyk <akowalchyk@zagmail.gonzaga.edu>
2021-08-10 16:51:50 -0700

    api key

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def generate():
     distance =  request.form.get('distance')
     address = str(address)
     distance = str(distance)
-    google_url, photo_url, name, type = "hello", "hi", "how", "you"
+    google_url, photo_url, name, type = get_random_place()
     return render_template('index.html', google_url = google_url, photo_url = photo_url, name = name, type = type)
 
 
